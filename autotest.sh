687eeead3298dc46041857c21941d0d4670815d3
boshining <1622732461@qq.com>
2019-05-27 06:17:09 -0700

    修改中文文本匹配无预训练

--- autotest.sh
+++ autotest.sh
@@ -5,7 +5,7 @@ else
 fi
 
 if [ "$1"x = "Y"x ]; then
-    for path in '--conf_path=autotest/conf/conf_text_classification_bilstm_attn_autotest.json --force=True' '--conf_path=autotest/conf/conf_chinese_text_matching_emb_char_autotest.json --force=True' '--conf_path=autotest/conf/conf_question_pairs_bilstm_attn_autotest.json --force=True' '--conf_path=autotest/conf/conf_kdtm_match_linearAttn_autotest.json --force=True'
+    for path in '--conf_path=autotest/conf/conf_text_classification_bilstm_attn_autotest.json --force=True' '--conf_path=autotest/conf/conf_chinese_text_matching_char_autotest.json --force=True' '--conf_path=autotest/conf/conf_question_pairs_bilstm_attn_autotest.json --force=True' '--conf_path=autotest/conf/conf_kdtm_match_linearAttn_autotest.json --force=True'
     do
         (
          CUDA_VISIBLE_DEVICES=$gpu_device python train.py $path
@@ -16,8 +16,8 @@ if [ "$1"x = "Y"x ]; then
 else
    CUDA_VISIBLE_DEVICES=$gpu_device python train.py --conf_path=autotest/conf/conf_text_classification_bilstm_attn_autotest.json --force=True
    CUDA_VISIBLE_DEVICES=-1 python test.py --conf_path=autotest/conf/conf_text_classification_bilstm_attn_autotest.json --force=True
-   CUDA_VISIBLE_DEVICES=$gpu_device python train.py --conf_path=autotest/conf/conf_chinese_text_matching_emb_char_autotest.json --force=True
-   CUDA_VISIBLE_DEVICES=-1 python test.py --conf_path=autotest/conf/conf_chinese_text_matching_emb_char_autotest.json --force=True
+   CUDA_VISIBLE_DEVICES=$gpu_device python train.py --conf_path=autotest/conf/conf_chinese_text_matching_char_autotest.json --force=True
+   CUDA_VISIBLE_DEVICES=-1 python test.py --conf_path=autotest/conf/conf_chinese_text_matching_char_autotest.json --force=True
    CUDA_VISIBLE_DEVICES=$gpu_device python train.py --conf_path=autotest/conf/conf_question_pairs_bilstm_attn_autotest.json --force=True
    CUDA_VISIBLE_DEVICES=-1 python test.py --conf_path=autotest/conf/conf_question_pairs_bilstm_attn_autotest.json --force=True
    CUDA_VISIBLE_DEVICES=$gpu_device python train.py --conf_path=autotest/conf/conf_kdtm_match_linearAttn_autotest.json --force=True
